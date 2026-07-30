4757d5280e1d2c2a0b09d4456b62048e44cd12e8
saiweekend <79636163+saiweekend@users.noreply.github.com>
2026-07-30 23:01:29 +0900

    Update script.js

--- src/script.js
+++ src/script.js
@@ -1,7 +1,7 @@
 // Sample project data
 const projects = [
   {
-      id: 7,
+      id: 1,
       title: "2026 · WITCH HAT ATELIER AR",
       image: "assets/img/audio/WHA.jpg", // Replace with actual image URL
       soundcloudEmbed: `<iframe width="560" height="315" src="https://www.youtube.com/embed/HjyKMGPfv7Q?si=CZjh69qfIdtI5UUS" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>`
@@ -9,21 +9,21 @@ const projects = [
   
   
   {
-      id: 7,
+      id: 2,
       title: "2026 · WOULD YOU LOVE ME",
       image: "assets/img/audio/WYLM.jpg", // Replace with actual image URL
       soundcloudEmbed: `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay; encrypted-media" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%253Atracks%253A2341904348&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/knd2" title="fua" target="_blank" style="color: #cccccc; text-decoration: none;">fua</a> · <a href="https://soundcloud.com/knd2/would-you-love-me-wav" title="WOULD YOU LOVE ME" target="_blank" style="color: #cccccc; text-decoration: none;">WOULD YOU LOVE ME</a></div>`
     },  
   
   {
-      id: 7,
+      id: 3,
       title: "2026 · Day Cycle",
       image: "assets/img/audio/daycycle.jpg", // Replace with actual image URL
       soundcloudEmbed: `<iframe data-testid="embed-iframe" style="border-radius:12px" src="https://open.spotify.com/embed/album/3xvcrul9WNHXusqOBLIatU?utm_source=generator&si=9e6c33a6ddea4774" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`
     },  
   
   {
-      id: 1,
+      id: 4,
       title: "2025 · itsgo1ng2be0k w/ recyclexbin",
       image: "assets/img/audio/itsgoingtobeok.png", // Replace with actual image URL
       soundcloudEmbed: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/4wki2cX85Gwe6DOttqGND9?utm_source=generator" width="100%" 
@@ -31,7 +31,7 @@ const projects = [
     },
     // Add more projects as needed
     {
-        id: 2,
+        id: 5,
         title: "2025 · FROM ME 2 U",
         soundcloudEmbed: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/7yHay8Z6F7FxUi0PTACVie?utm_source=generator&theme=0" 
         width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
@@ -40,7 +40,7 @@ const projects = [
     },
 
     {
-        id: 3,
+        id: 6,
         title: "2024 . ENSEMBLE",
         soundcloudEmbed: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/6KpLoYJT82I5ZARDJIMNaJ?utm_source=generator" 
         width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
@@ -49,7 +49,7 @@ const projects = [
     },
     
     {
-        id: 4,
+        id: 7,
         title: "2024 . ALABASTER & THE BASTARD SON",
         soundcloudEmbed: `<iframe style="border-radius:12px" src="https://open.spotify.com/embed/album/04TAaaEhlxIMpEi5DdnhrM?utm_source=generator" 
         width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>`,
@@ -58,7 +58,7 @@ const projects = [
     },
 
     {
-        id: 5,
+        id: 8,
         title: "2024 . SEPTEMBER",
         soundcloudEmbed: `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" 
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1925227388&color=%23444444&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
@@ -68,7 +68,7 @@ const projects = [
     },
 
     {
-        id: 6,
+        id: 9,
         title: "2024 . APRIL",
         soundcloudEmbed: `<iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" 
         src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1814096934&color=%23444444&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
@@ -78,7 +78,7 @@ const projects = [
     },
 
     {
-        id: 6,
+        id: 10,
         title: "2023 . BREAK FROM GRACE",
         soundcloudEmbed: `<iframe style="border-radius:12px" 
         src="https://open.spotify.com/embed/album/1jSbvC2b2vZYEcpRCu6hqw?utm_source=generator" 
@@ -88,7 +88,7 @@ const projects = [
     },
       
     {
-        id: 6,
+        id: 11,
         title: "2021 . PAIN IN POETRY",
         soundcloudEmbed: `<iframe style="border-radius:12px" 
         src="https://open.spotify.com/embed/album/59N7fvQYTb8doHa6YKAtmo?utm_source=generator" 
@@ -98,7 +98,7 @@ const projects = [
     },
 
     {
-        id: 6,
+        id: 12,
         title: "2024 - 2025 . COLLABORATIONS",
         soundcloudEmbed: `
         <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/0plHrAqS0VRyv9YBWxmq0s?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy">
@@ -114,7 +114,7 @@ const projects = [
     },
 
     {
-        id: 6,
+        id: 13,
         title: "MISC.",
         soundcloudEmbed: `<a href="https://www.patreon.com/c/knd33xo"> <img src="/assets/img/audio/ilovethecoloringbook.png"  alt="" > </a>`,
         image: "assets/img/78pxLogoCrossARC33xo.png",
